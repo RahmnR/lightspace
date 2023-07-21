209c1303bafb619829d5e4c3b88c18ccec092ed1
rrahman2298 <rrahman2298@gmail.com>
2023-07-22 00:10:17 +0700

    Create Entity and Repository

---
+++ src/main/java/com/enigma/lightspace/entity/Vendor.java
@@ -0,0 +1,47 @@
+package com.enigma.lightspace.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "m_vendor")
+public class Vendor {
+    @Id
+    @GenericGenerator(strategy = "uuid2", name = "system-uuid")
+    @GeneratedValue(generator = "system-uuid")
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone", unique = true)
+    private String phone;
+
+    @OneToOne
+    @JoinColumn(name = "user_credential_id")
+    private UserCredential userCredential;
+
+    @CreatedDate
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "status", nullable = false)
+    private Boolean status;
+}
